447f859bc740409773f278874ed1a547f2ea3827
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-07-20 12:51:13 +0200

    Deleted unused package and added new Circle CI configuration.

--- angular-cli/src/app/app.module.ts
+++ angular-cli/src/app/app.module.ts
@@ -2,13 +2,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AgGridModule} from 'ag-grid-angular/main';
 import {AppComponent} from "./app.component";
-import {MyGridApplicationComponent} from "./my-grid-application/my-grid-application.component";
 import {RedComponentComponent} from "./red-component/red-component.component";
 
 @NgModule({
     declarations: [
         AppComponent,
-        MyGridApplicationComponent,
         RedComponentComponent
     ],
     imports: [
