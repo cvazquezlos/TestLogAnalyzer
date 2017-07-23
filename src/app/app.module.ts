fe83d018643c167b0257b2d011f82223cc2a4e52
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-07-23 16:06:59 +0200

    Added bootstrap.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {AgGridModule} from 'ag-grid-angular/main';
 
 import {AppComponent} from './app.component';
@@ -11,6 +12,7 @@ import {RedComponentComponent} from './red-component/red-component.component';
         RedComponentComponent
     ],
     imports: [
+      NgbModule.forRoot(),
         BrowserModule,
         AgGridModule.withComponents(
             [RedComponentComponent]
