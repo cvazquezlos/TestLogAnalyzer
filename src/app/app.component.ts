d28ade9fbb3c2e6482c3c60f896cab3f0ed98c82
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-07-23 17:13:29 +0200

    Added filter.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,24 +10,24 @@ import {GridOptions} from 'ag-grid/main';
 })
 
 export class AppComponent {
+    showGrid: boolean;
     gridOptions: GridOptions;
     columnDefs: any[]
     rowData: any[];
 
     constructor() {
         this.gridOptions = <GridOptions>{};
-
         this.columnDefs = [
           {headerName: 'Make', field: 'make'},
           {headerName: 'Model', field: 'model', cellRendererFramework: RedComponentComponent},
           {headerName: 'Price', field: 'price'}
         ];
-
         this.rowData = [
           {make: 'Toyota', model: 'Celica', price: 35000},
           {make: 'Ford', model: 'Mondeo', price: 32000},
           {make: 'Porsche', model: 'Boxter', price: 72000}
-        ]
+        ];
+        this.showGrid = true;
     }
 
     onGridReady(params) {
@@ -36,5 +36,9 @@ export class AppComponent {
 
     selectAllRows() {
         this.gridOptions.api.selectAll();
+    }
+
+    private onQuickFilterChanged($event) {
+        this.gridOptions.api.setQuickFilter($event.target.value);
     }
 }
