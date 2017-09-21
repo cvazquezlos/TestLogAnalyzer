3f8b0eef05153f2089bb7d25a504ff2cb4bb5da2
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-09-21 11:00:39 +0200

    Added Covalent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import {AgGridModule} from 'ag-grid-angular/main';
-import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, Type} from '@angular/core';
 import {HttpModule, JsonpModule} from '@angular/http';
-import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {CovalentHighlightModule} from '@covalent/highlight';
+import {CovalentHttpModule, IHttpInterceptor} from '@covalent/http';
+import {CovalentMarkdownModule} from '@covalent/markdown';
+
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import {AgGridModule} from 'ag-grid-angular/main';
 
 import {AppComponent} from './app.component';
 import {ConfigComponent} from './component/config/config.component';
@@ -11,8 +17,13 @@ import {HeaderComponent} from './component/header/header.component';
 import {HomeComponent} from './component/home.component';
 
 import {routing} from './app.routing';
-
 import {ElasticsearchService} from './service/elasticsearch.service';
+
+import {RequestInterceptor} from '../config/interceptor/request.interceptor';
+
+const httpInterceptorProviders: Type<any>[] = [
+  RequestInterceptor,
+];
 
 @NgModule({
   declarations: [
@@ -26,6 +37,13 @@ import {ElasticsearchService} from './service/elasticsearch.service';
     AgGridModule.withComponents(
       [HomeComponent]
     ),
+    CovalentHttpModule.forRoot({
+      interceptors: [{
+        interceptor: RequestInterceptor, paths: ['**'],
+      }],
+    }),
+    CovalentHighlightModule,
+    CovalentMarkdownModule,
     BrowserModule,
     HttpModule,
     JsonpModule,
