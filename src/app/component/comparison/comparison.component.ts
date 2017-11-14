1fd2298384a07454b198ba9c857d6cd16d7f67c7
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-11-15 00:03:36 +0100

    Now a test cant be selected.

--- src/app/component/comparison/comparison.component.ts
+++ src/app/component/comparison/comparison.component.ts
@@ -12,100 +12,7 @@ import {ElasticsearchService} from '../../service/elasticsearch.service';
 export class ComparisonComponent {
 
   methods: Object[] = [];
-
-  items: Object[] = [{
-    color: 'deep-purple-A700',
-    icon: 'view_list',
-    route: 'steps',
-    title: 'Stepper',
-  }, {
-    color: 'blue-A700',
-    icon: 'open_with',
-    route: 'expansion-panel',
-    title: 'Expansion Panel',
-  }, {
-    color: 'pink-A700',
-    icon: 'space_bar',
-    route: 'file-input',
-    title: 'File Input',
-  }, {
-    color: 'cyan-A700',
-    icon: 'attach_file',
-    route: 'file-upload',
-    title: 'File Upload',
-  }, {
-    color: 'deep-orange-A700',
-    icon: 'label',
-    route: 'chips',
-    title: 'Chips',
-  }, {
-    color: 'lime-A700',
-    icon: 'hourglass_empty',
-    route: 'loading',
-    title: 'Loading',
-  }, {
-    color: 'amber-A700',
-    icon: 'open_in_browser',
-    route: 'dialogs',
-    title: 'Simple Dialogs',
-  }, {
-    color: 'green-A700',
-    icon: 'grid_on',
-    route: 'data-table',
-    title: 'Data Table',
-  }, {
-    color: 'orange-A700',
-    icon: 'format_line_spacing',
-    route: 'virtual-scroll',
-    title: 'Virtual Scroll',
-  }, {
-    color: 'teal-A700',
-    icon: 'format_indent_increase',
-    route: 'json-formatter',
-    title: 'JSON Formatter',
-  }, {
-    color: 'indigo-A700',
-    icon: 'swap_horiz',
-    route: 'paging',
-    title: 'Paging',
-  }, {
-    color: 'purple-A700',
-    icon: 'notifications',
-    route: 'notifications',
-    title: 'Notifications',
-  }, {
-    color: 'light-blue-A700',
-    icon: 'info_outline',
-    route: 'message',
-    title: 'Messages',
-  }, {
-    color: 'indigo-A700',
-    icon: 'search',
-    route: 'search',
-    title: 'Search',
-  }, {
-    color: 'red-A700',
-    icon: 'devices',
-    route: 'media',
-    title: 'Media Queries',
-  }, {
-    color: 'light-blue-A700',
-    icon: 'wb_iridescent',
-    route: 'directives',
-    title: 'Directives',
-  }, {
-    color: 'deep-orange-A700',
-    icon: 'filter_list',
-    route: 'pipes',
-    title: 'Pipes',
-  },
-    {
-      color: 'amber-A700',
-      icon: 'theaters',
-      route: 'animations',
-      title: 'Animations',
-    },
-  ];
+  selected = false;
 
   constructor(private elasticsearchService: ElasticsearchService) {
     this.initInfo('1');
@@ -134,6 +41,10 @@ export class ComparisonComponent {
       },
       error => console.log(error)
     );
+  }
+
+  private update() {
+    this.selected = true;
   }
 
 }
