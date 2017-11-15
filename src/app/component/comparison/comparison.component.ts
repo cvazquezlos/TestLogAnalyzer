e78146ae3c8601915d13a9187d7505baef0da501
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-11-15 23:21:35 +0100

    Added default navbar.

--- src/app/component/comparison/comparison.component.ts
+++ src/app/component/comparison/comparison.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy,
+  Component} from '@angular/core';
+import { TdMediaService } from '@covalent/core';
 
 import {Log} from '../../model/source.model';
 
@@ -10,11 +12,73 @@ import {ElasticsearchService} from '../../service/elasticsearch.service';
 })
 
 export class ComparisonComponent {
-
+  routes: Object[] = [{
+    icon: 'home',
+    route: '.',
+    title: 'Home',
+  }, {
+    icon: 'library_books',
+    route: '.',
+    title: 'Documentation',
+  }, {
+    icon: 'color_lens',
+    route: '.',
+    title: 'Style Guide',
+  }, {
+    icon: 'view_quilt',
+    route: '.',
+    title: 'Layouts',
+  }, {
+    icon: 'picture_in_picture',
+    route: '.',
+    title: 'Components & Addons',
+  },
+  ];
+  usermenu: Object[] = [{
+    icon: 'swap_horiz',
+    route: '.',
+    title: 'Switch account',
+  }, {
+    icon: 'tune',
+    route: '.',
+    title: 'Account settings',
+  }, {
+    icon: 'exit_to_app',
+    route: '.',
+    title: 'Sign out',
+  },
+  ];
+  navmenu: Object[] = [{
+    icon: 'looks_one',
+    route: '.',
+    title: 'First item',
+    description: 'Item description',
+  }, {
+    icon: 'looks_two',
+    route: '.',
+    title: 'Second item',
+    description: 'Item description',
+  }, {
+    icon: 'looks_3',
+    route: '.',
+    title: 'Third item',
+    description: 'Item description',
+  }, {
+    icon: 'looks_4',
+    route: '.',
+    title: 'Fourth item',
+    description: 'Item description',
+  }, {
+    icon: 'looks_5',
+    route: '.',
+    title: 'Fifth item',
+    description: 'Item description',
+  },
+  ];
   methods: Object[] = [];
   selected = false;
 
-  constructor(private elasticsearchService: ElasticsearchService) {
+  constructor(private elasticsearchService: ElasticsearchService, public media: TdMediaService) {
     this.initInfo('1');
   }
 
