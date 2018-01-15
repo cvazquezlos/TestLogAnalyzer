18e1d3c0c6c050b891a66dd56bd16559e106011d
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2018-01-15 19:12:18 +0100

    Some changes. Comparison method doesn't work.

--- src/app/component/comparison/comparison.component.ts
+++ src/app/component/comparison/comparison.component.ts
@@ -29,6 +29,7 @@ export class ComparisonComponent {
   methods: any[] = [];
   mode = 0;
   results = [];
+  richTextDiff = require('../../../../node_modules/rich-text-diff');
 
   constructor(private elasticsearchService: ElasticsearchService, public media: TdMediaService,
               private diffService: DiffService, private execStatusService: ExecsStatusService) {
@@ -38,16 +39,26 @@ export class ComparisonComponent {
 5 [main] INFO com.example.bookstore.basic.BasicUnitTest Database must contain any target value.
 5 [main] INFO com.example.bookstore.basic.BasicUnitTest After deleting info, database size have to be 19.
 5 [main] INFO com.example.bookstore.basic.BasicUnitTest Database can't contain target value.`;
-    const str2 = `5 [main] INFO com.example.bookstore.basic.BasicUnitTest Database must contain any target value.
+    const str2 = `0 [main] INFO com.example.bookstore.basic.BasicUnitTest Database must contain any target value.
 5 [main] INFO com.example.bookstore.basic.BasicUnitTest After deleting info, database size have to be 19.
 6 [main] INFO com.example.bookstore.basic.BasicUnitTest Database can't contain target value.`;
-    console.log('Hola' + richTextDiff(str1, str2));
+    const str3 = `2017-12-28 13:58:23.069 [main] INFO com.example.bookstore.basic.BasicUnitTest Database can't be null.
+2017-12-28 13:58:23.074 [main] INFO com.example.bookstore.basic.BasicUnitTest Database must contain any target value.
+2017-12-28 13:58:23.074 [main] INFO com.example.bookstore.basic.BasicUnitTest After deleting info, database size have to be 19.
+2017-12-28 13:58:23.074 [main] INFO com.example.bookstore.basic.BasicUnitTest Database can't contain target value.`;
+    const str4 = `2017-11-19 00:24:43.881 [main] INFO com.example.bookstore.basic.BasicUnitTest Database must contain any target value.
+2017-11-19 00:24:43.886 [main] INFO com.example.bookstore.basic.BasicUnitTest After deleting info, database size have to be 19.
+2017-11-19 00:24:43.887 [main] INFO com.example.bookstore.basic.BasicUnitTest Database can't contain target value.`;
+    console.log('Time primero ejecucion 3 diff: ' + richTextDiff(str1, str2));
+    console.log('Time primero ejecucion 1 diff: ' + richTextDiff(str2, str1));
+    console.log('Normal primero ejecucion 3: ' + richTextDiff(str3, str4));
+    console.log('Normal primero ejecucion 1: ' + richTextDiff(str4, str3));
   }
 
   generateComparison() {
     this.loadInfo(localStorage.getItem('CExecI'), localStorage.getItem('CExecM'), '2 0');
     this.loadInfo(localStorage.getItem('cExecI'), localStorage.getItem('cExecM'), '2 1');
-    this.results = this.diffService.generateComparison(this.process.nativeElement.innerHTML.toString());
+    this.results = this.diffService.generateComparison(this.richTextDiff(this.comparatorText, this.comparedText));
     this.comparison = true;
   }
 
--- src/app/service/diff.service.ts
+++ src/app/service/diff.service.ts
@@ -11,8 +11,29 @@ export class DiffService {
   results: any[];
 
   generateComparison(diff: string) {
-    console.log(diff.split('<br>'));
-    let lines = this.correctDelMistakes(diff.split('<br>'), ['<del>', '</del>']);
+    let lines = diff.split('\n');
+    console.log(lines);
+    let j, k, comparatorLine, comparedLine, c1, c2: any;
+    this.results = [];
+    this.resetIterator();
+    lines.pop();
+    lines.forEach(line => {
+      console.log(line.length);
+      console.log(line.lastIndexOf('</del>'));
+      j = this.iteratorContent.j;
+      k = this.iteratorContent.k;
+      comparatorLine = this.deleteUselessData(line, '<ins>', '</ins>', 0);
+      comparedLine = this.deleteUselessData(line, '<del>', '</del>', 1);
+      (comparatorLine.length < (line.length * 0.3)) ? (this.updateIndexes(comparatorLine, '', '',
+        comparedLine, j, k + 1, this.iteratorContent.i1, k.toString() + '.', 0)) : (c1 = true);
+      (comparedLine.length < (line.length * 0.3)) ? (this.updateIndexes('', comparedLine, comparatorLine,
+        '', j + 1, k, j.toString() + '.', this.iteratorContent.i2, 1)) : (c2 = true);
+      if (c1 && c2) {
+        this.updateIndexes('', '', comparatorLine, comparedLine, j + 1, k + 1, j.toString() + '.', k.toString() + '.');
+      }
+      this.concatResults(this.iteratorContent.i1, this.iteratorContent.i2, this.iteratorContent.line1, this.iteratorContent.line2);
+    });
+    /*
     console.log('After clean del' + lines);
     lines = this.correctInsMistakes(lines, ['<ins>', '</ins>']);
     console.log('After clean ins' + lines);
@@ -33,47 +54,13 @@ export class DiffService {
         this.updateIndexes('', '', comparatorLine, comparedLine, j + 1, k + 1, j.toString() + '.', k.toString() + '.');
       }
       this.concatResults(this.iteratorContent.i1, this.iteratorContent.i2, this.iteratorContent.line1, this.iteratorContent.line2);
-    });
+    });*/
     return this.results;
   }
 
-  private correctDelMistakes(lines: any[], t1: string[]) {
-    for (let i = 0; i < lines.length; i++) {
-      console.log(lines[i]);
-      if (lines[i].lastIndexOf(t1[0]) > lines[i].lastIndexOf(t1[1])) {
-        console.log('First condition');
-        lines[i] = lines[i] + t1[1];
-        this.active = true;
-      } else if (this.active) {
-        console.log('Second condition');
-        lines[i] = t1[0] + lines[i];
-        this.active = false;
-      }
-      console.log(lines[i]);
-    }
-    return lines;
-  }
-
-  private correctInsMistakes(lines: any[], t1: string[]) {
-    for (let i = 0; i < lines.length; i++) {
-      console.log(lines[i]);
-      if (lines[i].lastIndexOf(t1[0]) > lines[i].lastIndexOf(t1[1])) {
-        console.log('First condition');
-        lines[i] = lines[i] + t1[1];
-        this.active = true;
-      } else if (this.active) {
-        console.log('Second condition');
-        lines[i] = t1[0] + lines[i];
-        this.active = false;
-      }
-      console.log(lines[i]);
-    }
-    return lines;
-  }
-
   generateOutput(log: Log) {
-    return (log.timestamp + ' [' + log.thread_name + '] ' + log.level + ' ' + log.logger_name + '' +
-      ' ' + log.formatted_message) + '\n';
+    return log.timestamp + ' [' + log.thread_name + '] ' + log.level + ' ' + log.logger_name + '' +
+      ' ' + log.formatted_message + '\n';
   }
 
   noTimestampDiff(data: any[]) {
