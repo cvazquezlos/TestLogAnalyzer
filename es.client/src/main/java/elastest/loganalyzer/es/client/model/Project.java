a8a500d39e0a3ddd435173f037bf78893cf09afd
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2018-01-27 20:57:28 +0100

    Added new model to support projects creation.

---
+++ es.client/src/main/java/elastest/loganalyzer/es/client/model/Project.java
@@ -0,0 +1,37 @@
+package elastest.loganalyzer.es.client.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName = "projects", type ="project")
+public class Project {
+
+	@Id
+	private String id;
+	
+	private int num_execs;
+	
+	public Project() {
+	}
+	
+	public Project(String id, int num_execs) {
+		this.id = id;
+		this.num_execs = num_execs;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getNum_execs() {
+		return num_execs;
+	}
+
+	public void setNum_execs(int num_execs) {
+		this.num_execs = num_execs;
+	}
+}
