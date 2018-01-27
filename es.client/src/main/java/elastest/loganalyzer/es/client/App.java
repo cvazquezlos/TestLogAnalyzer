1eed6f6ccf48e822f753b8abc33a6cf3b13a8a3a
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2018-01-27 19:46:08 +0100

    Added default Spring project.

---
+++ es.client/src/main/java/elastest/loganalyzer/es/client/App.java
@@ -0,0 +1,13 @@
+package elastest.loganalyzer.es.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
