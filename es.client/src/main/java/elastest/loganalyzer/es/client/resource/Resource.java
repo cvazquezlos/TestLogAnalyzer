beca10f9749b01d83fb490a14a37bcf448f6e0f5
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2018-02-02 15:59:18 +0100

    Upload info to cluster working.

--- es.client/src/main/java/elastest/loganalyzer/es/client/resource/Resource.java
+++ es.client/src/main/java/elastest/loganalyzer/es/client/resource/Resource.java
@@ -23,27 +23,20 @@ import elastest.loganalyzer.es.client.service.ExecutionParserService;
 
 @RestController
 @RequestMapping("/files")
-@Import(EsConfiguration.class)
 public class Resource {
 	
 	private final ESProjectService esProjectService;
 	private final ESLogService esLogService;
 	private final ExecutionParserService executionParserService;
-	private final ApplicationContext context;
 	
 	private static String recentProject;
-	private Index index;
 
 	@Autowired
 	public Resource(ESProjectService esProjectService, ExecutionParserService executionParserService, ESLogService esLogService) {
 		this.esProjectService = esProjectService;
 		this.esLogService = esLogService;
 		this.executionParserService = executionParserService;
-	    context = new ClassPathXmlApplicationContext("applicationContext.xml");
 	}
-
-	@Autowired
-	private ElasticsearchOperations elasticsearchTemplate;
 
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
 	public String upload(@RequestBody MultipartFile file) {
@@ -51,10 +44,8 @@ public class Resource {
 			if (file != null) {
 				Project target = esProjectService.findByName(recentProject);
 				this.executionParserService.parse(file, target, Lists.newArrayList(esLogService.findAll()).size());
-				
-				//elasticsearchTemplate.createIndex(indexName);
-				//elasticsearchTemplate.putMapping(indexName, "logs", elasticsearchTemplate.getMapping("loganalyzer", "logs"));
-				System.out.println((new String(file.getBytes(), "UTF-8")).split(System.getProperty("line.separator")));
+				target.setNum_execs(target.getNum_execs() + 1);
+				esProjectService.save(target);
 			} else {
 				System.out.println("Fail");
 			}
