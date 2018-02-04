89050498977a402fb4d86a44e9acf93f720750fc
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2018-02-04 17:44:31 +0100

    Added control of errors in add-project form.

--- src/app/component/add-project/add-project.component.ts
+++ src/app/component/add-project/add-project.component.ts
@@ -1,7 +1,5 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Project} from '../../model/project.model';
 import {ElasticsearchService} from '../../service/elasticsearch.service';
@@ -14,27 +12,27 @@ import {ElasticsearchService} from '../../service/elasticsearch.service';
 
 export class AddProjectComponent {
 
+  code: number;
   fileSelected: boolean;
   fileTxt: File;
   fileXml: File;
-  urlTxt: string;
-  urlXml: string;
   isFile: boolean;
   project: Project;
-  updatingFile: boolean;
+  urlTxt: string;
+  urlXml: string;
 
-  constructor(private elasticsearchService: ElasticsearchService, private http: HttpClient) {
+  constructor(private elasticsearchService: ElasticsearchService, private http: HttpClient, private router: Router) {
+    this.code = 0;
+    this.fileSelected = true;
+    this.fileTxt = null;
+    this.fileXml = null;
+    this.isFile = true;
+    this.urlTxt = '';
+    this.urlXml = '';
     this.project = new Project();
     this.project.name = '';
     this.elasticsearchService.countProjects().subscribe(response => this.project.id = response);
     this.project.num_execs = 0;
-    this.isFile = true;
-    this.urlTxt = '';
-    this.urlXml = '';
-    this.fileSelected = true;
-    this.fileTxt = null;
-    this.fileXml = null;
-    this.updatingFile = false;
   }
 
   cancel() {
@@ -43,7 +41,7 @@ export class AddProjectComponent {
   }
 
   save() {
-    this.updatingFile = true;
+    this.code = 1;
     this.elasticsearchService.postProject(this.project).subscribe(
       response => {
         let headers: HttpHeaders = new HttpHeaders();
@@ -55,7 +53,7 @@ export class AddProjectComponent {
             formData.append('file', this.fileTxt);
             this.http.post('http://localhost:8443/files/upload', formData, {headers: headers}).subscribe(
               result2 => {
-                this.updatingFile = false;
+                this.code = 2;
               }
             );
           },
@@ -69,6 +67,10 @@ export class AddProjectComponent {
     (file.name.includes('.txt')) ? (this.fileTxt = file) : (this.fileXml = file);
     this.urlTxt = 'Empty';
     this.urlXml = 'Empty';
+  }
+
+  private returnHome() {
+    this.router.navigateByUrl('/');
   }
 
 }
