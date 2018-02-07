e90d97c53d63f4164b25173150d01a0edff00ce2
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2018-02-07 17:58:09 +0100

    Fullscreen working. Ready to merge.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,36 @@ export class AppComponent {
     this._iconRegistry.addSvgIconInNamespace('assets', 'github',
       this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/github.svg'));
   }
+
+  toggleFullscreen() {
+    let documentVar = document as any;
+    let documentElement = document.documentElement as any;
+    let documentBody = document.body as any;
+
+    if (documentVar.fullscreenElement || // alternative standard method
+      documentVar.mozFullScreenElement || // currently working methods
+      documentVar.webkitFullscreenElement ||
+      documentVar.msFullscreenElement) {
+      if (documentVar.exitFullscreen) {
+        documentVar.exitFullscreen();
+      } else if (documentVar.mozCancelFullScreen) {
+        documentVar.mozCancelFullScreen();
+      } else if (documentVar.webkitExitFullscreen) {
+        documentVar.webkitExitFullscreen();
+      } else if (documentVar.msExitFullscreen) {
+        documentVar.msExitFullscreen();
+      }
+    } else {
+      let element = Element as any;
+      if (documentElement.requestFullscreen) {
+        documentElement.requestFullscreen();
+      } else if (documentElement.mozRequestFullScreen) {
+        documentElement.mozRequestFullScreen();
+      } else if (documentElement.webkitRequestFullscreen) {
+        documentElement.webkitRequestFullscreen(element.ALLOW_KEYBOARD_INPUT);
+      } else if (documentBody.msRequestFullscreen) {
+        documentBody.msRequestFullscreen();
+      }
+    }
+  }
 }
