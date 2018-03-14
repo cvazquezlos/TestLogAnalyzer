b688db8996a558ee0d4fa387df0470edbeeac06d
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2018-03-14 23:20:41 +0100

    Solved Project class error.

--- es.client/src/main/java/elastest/loganalyzer/es/client/model/Project.java
+++ es.client/src/main/java/elastest/loganalyzer/es/client/model/Project.java
@@ -1,5 +1,7 @@
 package elastest.loganalyzer.es.client.model;
 
+import java.util.ArrayList;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
@@ -9,16 +11,20 @@ public class Project {
 	@Id
 	private int id;
 
+	private ArrayList<Integer> assigned_ids;
 	private String name;
 	private int num_execs;
+	private int recently_deleted;
 
 	public Project() {
 	}
 
-	public Project(int id, String name, int num_execs) {
+	public Project(int id, String name, int num_execs, ArrayList<Integer> assigned_ids, int recently_deleted) {
 		this.id = id;
+		this.assigned_ids = assigned_ids;
 		this.name = name;
 		this.num_execs = num_execs;
+		this.recently_deleted = recently_deleted;
 	}
 
 	public int getId() {
@@ -27,6 +33,14 @@ public class Project {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public ArrayList<Integer> getAssigned_ids() {
+		return assigned_ids;
+	}
+
+	public void setAssigned_ids(ArrayList<Integer> assigned_ids) {
+		this.assigned_ids = assigned_ids;
 	}
 
 	public String getName() {
@@ -43,5 +57,13 @@ public class Project {
 
 	public void setNum_execs(int num_execs) {
 		this.num_execs = num_execs;
+	}
+
+	public int getRecently_deleted() {
+		return recently_deleted;
+	}
+
+	public void setRecently_deleted(int recently_deleted) {
+		this.recently_deleted = recently_deleted;
 	}
 }
