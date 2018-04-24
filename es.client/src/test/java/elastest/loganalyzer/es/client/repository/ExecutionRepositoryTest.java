b2d2784a921fc0ce80a1f9e12a9171d18f13bc97
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2018-04-24 10:21:10 +0200

    Added DiffMatchPatch tests and fixed execution.

--- es.client/src/test/java/elastest/loganalyzer/es/client/repository/ExecutionRepositoryTest.java
+++ es.client/src/test/java/elastest/loganalyzer/es/client/repository/ExecutionRepositoryTest.java
@@ -13,8 +13,8 @@ import org.apache.maven.plugins.surefire.report.ReportTestCase;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -27,7 +27,7 @@ public class ExecutionRepositoryTest {
 
 	@Autowired
 	private ExecutionRepository repository;
-	private final Logger log = LoggerFactory.getLogger(ExecutionRepositoryTest.class);
+	// private final Logger log = LoggerFactory.getLogger(ExecutionRepositoryTest.class);
 
 	@Before
 	public void pre() {
@@ -40,24 +40,29 @@ public class ExecutionRepositoryTest {
 				"BUILD SUCCESS", "Demo", 3, "01", new ArrayList<ReportTestCase>(), (float) 20.3);
 		Execution e2 = new Execution(99999999, 43, 3, 4, 0, "JUnit4ClassTestingTLA", 3, new Date().toString(),
 				"BUILD FAILURE", "Demo", 2, "01", new ArrayList<ReportTestCase>(), (float) 603.2);
-		log.info("e2 entity created: " + e2.toString());
 		// When
 		Execution returnedValue1 = repository.save(e1);
 		Execution returnedValue2 = repository.save(e2);
 		// Then
 		assertEquals(returnedValue1, e1);
 		assertEquals(returnedValue2, e2);
+		repository.delete(e1);
+		repository.delete(e2);
 	}
 
 	@Test
 	public void ShouldFindAnyValueByProject() {
 		// Given
+		Execution e1 = new Execution(99999997, 123, 2, 27, 1, "JUnit4ClassTestingTLA", 2, new Date().toString(),
+				"BUILD UNKNOWN", "Demo", 3, "02", new ArrayList<ReportTestCase>(), (float) 0.3);
 		String project = "JUnit4ClassTestingTLA";
 		// When
+		repository.save(e1);
 		List<Execution> executions = repository.findByProject(project);
 		// Then
 		assertNotNull(executions);
-		assertEquals(executions.size(), 2);
+		assertEquals(executions.size(), 1);
+		repository.delete(e1);
 	}
 
 	@Test
@@ -72,21 +77,39 @@ public class ExecutionRepositoryTest {
 	@Test
 	public void ShouldFindAnyValueByProjectAndTab() {
 		// Given
+		Execution e1 = new Execution(99999996, 123, 13, 2, 1, "JUnit4ClassTestingTLA", 2, new Date().toString(),
+				"BUILD UNKNOWN", "Demo", 3, "02", new ArrayList<ReportTestCase>(), (float) 0.3);
+		Execution e2 = new Execution(99999995, 34, 20, 59, 6, "JUnit4ClassTestingTLA", 3, new Date().toString(),
+				"BUILD UNKNOWN", "Demo", 3, "02", new ArrayList<ReportTestCase>(), (float) 80.6);
+		Execution e3 = new Execution(99999994, 305, 45, 30, 2, "JUnit4ClassTestingTLA", 0, new Date().toString(),
+				"BUILD UNKNOWN", "Demo", 3, "02", new ArrayList<ReportTestCase>(), (float) 2.0);
 		String project = "JUnit4ClassTestingTLA";
 		String tab = "Demo";
 		// When
+		repository.save(e1);
+		repository.save(e2);
+		repository.save(e3);
 		List<Execution> executions = repository.findByProjectAndTabOrderById(project, tab);
 		// Then
 		assertNotNull(executions);
-		assertEquals(executions.size(), 2);
+		assertEquals(executions.size(), 3);
+		repository.delete(e1);
+		repository.delete(e2);
+		repository.delete(e3);
 	}
 
 	@Test
 	public void ShouldDeleteById() {
 		// Given
-		int id1 = 99999998;
-		int id2 = 99999999;
+		Execution e1 = new Execution(99999993, 201, 23, 2, 34, "JUnit4ClassTestingTLA", 0, new Date().toString(),
+				"BUILD SUCCESS", "Demo", 3, "01", new ArrayList<ReportTestCase>(), (float) 20.3);
+		Execution e2 = new Execution(99999992, 43, 3, 4, 0, "JUnit4ClassTestingTLA", 3, new Date().toString(),
+				"BUILD FAILURE", "Demo", 2, "01", new ArrayList<ReportTestCase>(), (float) 603.2);
+		int id1 = 99999993;
+		int id2 = 99999992;
 		// When
+		repository.save(e1);
+		repository.save(e2);
 		repository.delete(id1);
 		repository.delete(id2);
 		// Then
@@ -97,13 +120,21 @@ public class ExecutionRepositoryTest {
 	@Test
 	public void ShouldDontExist() {
 		// Given
-		int id1 = 99999998;
-		int id2 = 99999999;
+		Execution e1 = new Execution(99999996, 123, 13, 2, 1, "JUnit4ClassTestingTLA", 2, new Date().toString(),
+				"BUILD UNKNOWN", "Demo", 3, "02", new ArrayList<ReportTestCase>(), (float) 0.3);
+		Execution e2 = new Execution(99999995, 34, 20, 59, 6, "JUnit4ClassTestingTLA", 3, new Date().toString(),
+				"BUILD UNKNOWN", "Demo", 3, "02", new ArrayList<ReportTestCase>(), (float) 80.6);
+		int id1 = 99999996;
+		int id2 = 99999995;
 		// When
+		repository.save(e1);
+		repository.save(e2);
 		boolean v1 = repository.exists(id1);
 		boolean v2 = repository.exists(id2);
 		// Then
 		assertFalse(v1);
 		assertFalse(v2);
+		repository.delete(e1);
+		repository.delete(e2);
 	}
 }
