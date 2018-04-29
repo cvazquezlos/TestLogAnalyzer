319ec2e7055a92ee8f360bcc599b6828d82b4c9c
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2018-04-29 11:27:24 +0200

    Elasticsearch service tests finished.

--- src/app/service/elasticsearch.service.spec.ts
+++ src/app/service/elasticsearch.service.spec.ts
@@ -1,14 +1,76 @@
-import {TestBed, inject} from '@angular/core/testing';
+import {TestBed, inject, getTestBed, async} from '@angular/core/testing';
 import {ElasticsearchService} from './elasticsearch.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {Project} from '../model/project.model';
 
 describe('Service: Elasticsearch', () => {
+  let httpMock: HttpTestingController;
+  let injector: TestBed;
+  let service: ElasticsearchService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [ElasticsearchService]
-    })
+    });
+    injector = getTestBed();
+    service = injector.get(ElasticsearchService);
+    httpMock = injector.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('Should create the service', inject([ElasticsearchService], (service: ElasticsearchService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('Should return the number of the projects', () => {
+    service.getCountOfProjects().subscribe(
+      response => {
+        expect(typeof response).toEqual("number");
+      }
+    );
+  });
+
+  it('Should return all the projects', () => {
+    service.getProjectsAll().subscribe(
+      response => {
+        expect(response.length).toBeGreaterThan(-1);
+      }
+    );
+  });
+
+  it('Should return an error when calling a no existing project', () => {
+    service.getProjectByName('JasmineTestingName').subscribe(
+      () => {},
+      error => {
+        expect(error).toBe('No project found with the given name.')
+      }
+    )
+  });
+
+  it('Should create a project and then delete it', async () => {
+    const project = new Project();
+    expect(project.assigned_ids.length).toBe(0);
+    expect(project.name).toBe('');
+    project.name = 'JasmineTestingProject';
+    expect(project.name).toBe('JasmineTestingProject');
+    project.id = 99999999;
+    const response = await service.postProject(project);
+    expect(response).toBe(99999999);
+    service.deleteProjectById(99999999).subscribe(
+      response => {
+        expect(response).toBe(project);
+      }
+    );
+  });
+
+  it('Should return a diff', async() => {
+    const text1 = 'This is a demo text';
+    const text2 = 'Thise are a demou test';
+    const response = await service.postDiff(text1, text2);
+    expect(response.length).toBeGreaterThanOrEqual(0);
+  });
 });
